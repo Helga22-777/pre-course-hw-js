b0310e3da5d6770d977ca000681f90b2d3cc3bef
Helga22-777 <kisa.manenka@gmail.com>
2024-09-13 20:17:02 +0300

    add 05js-5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,13 +1,16 @@
 let rule = "Еще не родился тот";
+let rule2 =
+  "Еще не родился тот человек, который, поставил бы цель, и не смог бы стать программистом.";
+let rule3 = "Еще не родился тот челове";
 
 switch (true) {
-  case rule.length === 25:
+  case rule3.length === 25:
     console.log("50 на 50");
     break;
   case rule.length < 25:
     console.log("Все таки нет правил без исключения");
     break;
-  case rule.length > 25:
+  case rule2.length > 25:
     console.log("У меня 100 пудов все получится");
     break;
   default:
